6928f6c618a78243c434086fe039b03ec9636f05
Richard Mustaklem <rmustak@iu.edu>
2024-11-04 14:16:32 -0500

    Update Tune_Markers.R

--- R/Tune_Markers.R
+++ R/Tune_Markers.R
@@ -15,29 +15,51 @@
 #'
 #' @export
 
-Tune_Markers = function(ann, method, method_value, method_var,set){
-   if (is.null(ann@ann1[[set]])) {
-      print("WARNING: Markers for the given set have not been initialized. Please run Initialize_Markers() prior to tuning with this funciton.")
-   }else{
+Tune_Markers = function(ann, method, method_value, method_var, set) {
+   if (is.null(ann@ann1[[set]])) { #and statement here, if method %in% colnames(ann@ann1[[set]])
+      warning("Either Initialize_Markers() wasn't run or the specificed method does not exist for the set. Please refer to documentation.")
+   } else {
       if (method == "absolute") {
          tune_subset <- ann@ann1[[set]] %>%
-            group_by(cluster)%>%
-            top_n(method_value, wt = !!sym(method_var))%>%
+            group_by(cluster) %>%
+            top_n(method_value, wt = !!sym(method_var)) %>%
             ungroup()
 
-      }else if (method == "relative") {
+      } else if (method == "relative") {
          tune_subset <- ann@ann1[[set]] %>%
             group_by(cluster) %>%
             filter(!!sym(method_var) >= quantile(!!sym(method_var), probs = method_value)) %>%
             ungroup()
-      }else{
-         print("Method not supported. Please check documentation ?Tune_Markers().")
-      }
-   }
-   input_summary <- aggregate(gene~cluster, ann@ann1[[set]], length)
-   output_summary <- aggregate(gene~cluster, tune_subset, length)
-  cat(paste0("By running Tune_Markers() you have lowered the median markers per cluster from ", summary(input_summary$gene)[3], " to ",summary(output_summary$gene)[3],"."))
-   ann@ann1[[set]]=data.frame(tune_subset)
-   return(ann)
 
+      } else {
+         print("Method not supported. Please check documentation ?Tune_Markers().")
+         return(ann)
+      }
+
+      # Save the method parameters as a tuning entry in the history
+      tuning_entry <- list(
+         method = method,
+         method_value = method_value,
+         method_var = method_var,
+         set = set
+      )
+
+      # Append to tuning history, creating it if it doesn't exist
+      if (is.null(ann@params$markers$tuning_history)) {
+         ann@params$markers$tuning_history <- list(tuning_entry)
+      } else {
+         ann@params$markers$tuning_history <- append(ann@params$markers$tuning_history, list(tuning_entry))
+      }
+
+      # Assign tuned markers to the object
+      ann@ann1[[set]] <- data.frame(tune_subset)
+   }
+
+   # Print summary information
+   input_summary <- aggregate(gene ~ cluster, ann@ann1[[set]], length)
+   output_summary <- aggregate(gene ~ cluster, tune_subset, length)
+   cat(paste0("By running Tune_Markers() you have lowered the median markers per cluster from ",
+              summary(input_summary$gene)[3], " to ", summary(output_summary$gene)[3], "."))
+
+   return(ann)
 }
