9fbc08526dd8c576fdb7616a06e74004045c1905
Liam Horan-Portelance <142847439+lhoranportelance@users.noreply.github.com>
2024-11-14 15:57:30 -0500

    Update Generate_SAHA_Report.R

--- R/Generate_SAHA_Report.R
+++ R/Generate_SAHA_Report.R
@@ -1,5 +1,5 @@
 Generate_SAHA_Report <- function(SAHA, auto=NULL, output_file) {
   # render the .Rmd file
-  rmarkdown::render(input = system.file("extdata", "report_html.rmd", package = "SAHAdata"),
+  rmarkdown::render(input = system.file("extdata", "report_html.Rmd", package = "SAHAdata"),
                     output_file = output_file)
 }
