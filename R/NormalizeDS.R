b3fa9c55716656ddfab0fa4bc94fae943a13b231
Dom Acri <46611562+domjacri@users.noreply.github.com>
2025-01-13 11:55:31 -0500

    Adding capabilities to NormalizeDS
    Can now normalize "across_clust" (percentile expression across all) or "within_clust" (cluster respective z_score)

--- R/NormalizeDS.R
+++ R/NormalizeDS.R
@@ -16,88 +16,108 @@
 #' @importFrom stats scale, pnorm
 #'
 #' @export
-NormalizeDS <- function(ann,assay_db = "RNA",assay_query){
-   #1. Pivot to a new table - query
-   piv_query <- ann@ann3$query %>%
-      tibble::rownames_to_column(var = "Row") %>%
-      pivot_longer(cols = -Row, names_to = "cluster", values_to = "Expression")
+NormalizeDS <- function(ann,assay_db = "RNA",assay_query,norm_method = "across_clust"){
+   if (norm_method == "across_clust") {  #1. Pivot to a new table - query
+      piv_query <- ann@ann3$query %>%
+         tibble::rownames_to_column(var = "Row") %>%
+         pivot_longer(cols = -Row, names_to = "cluster", values_to = "Expression")
 
-   # average and make percentiles
+      # average and make percentiles
 
-   grouped_data_query <- split(piv_query, piv_query$Row)
+      grouped_data_query <- split(piv_query, piv_query$Row)
 
-   zscore_query <- lapply(grouped_data_query, function(gene_data) {
-      gene_data$z_score <- scale(gene_data$Expression)
-      return(gene_data)
-   })
+      zscore_query <- lapply(grouped_data_query, function(gene_data) {
+         gene_data$z_score <- scale(gene_data$Expression)
+         return(gene_data)
+      })
 
-   avgexp_query.df <- do.call(rbind, zscore_query)
-   avgexp_query.df$z_score <- as.numeric(avgexp_query.df$z_score)
-   avgexp_query.df$percentile <- pnorm(avgexp_query.df$z_score)
+      avgexp_query.df <- do.call(rbind, zscore_query)
+      avgexp_query.df$z_score <- as.numeric(avgexp_query.df$z_score)
+      avgexp_query.df$percentile <- pnorm(avgexp_query.df$z_score)
 
-   #2. Pivot to a new table - db
-   piv_db <- ann@ann3$db %>%
-      tibble::rownames_to_column(var = "Row") %>%
-      pivot_longer(cols = -Row, names_to = "celltype", values_to = "Expression")
+      #2. Pivot to a new table - db
+      piv_db <- ann@ann3$db %>%
+         tibble::rownames_to_column(var = "Row") %>%
+         pivot_longer(cols = -Row, names_to = "celltype", values_to = "Expression")
 
-   # average and make percentiles
+      # average and make percentiles
 
-   grouped_data_db <- split(piv_db, piv_db$Row)
+      grouped_data_db <- split(piv_db, piv_db$Row)
 
-   zscore_db <- lapply(grouped_data_db, function(gene_data) {
-      gene_data$z_score <- scale(gene_data$Expression)
-      return(gene_data)
-   })
+      zscore_db <- lapply(grouped_data_db, function(gene_data) {
+         gene_data$z_score <- scale(gene_data$Expression)
+         return(gene_data)
+      })
 
-   avgexp_db.df <- do.call(rbind, zscore_db)
-   avgexp_db.df$z_score <- as.numeric(avgexp_db.df$z_score)
-   avgexp_db.df$percentile <- pnorm(avgexp_db.df$z_score)
+      avgexp_db.df <- do.call(rbind, zscore_db)
+      avgexp_db.df$z_score <- as.numeric(avgexp_db.df$z_score)
+      avgexp_db.df$percentile <- pnorm(avgexp_db.df$z_score)
 
-   #3. making comparison b/w query and ABC expression profiles for major cell types
+      #3. making comparison b/w query and ABC expression profiles for major cell types
 
-   avgexp_db.df$celltype <- gsub(paste0("^",assay_db,"\\.g"), "", avgexp_db.df$celltype) ###########!!!!!!!!!!!need to print "Assay" into here!!
-   avgexp_query.df$cluster <- gsub(paste0("^",assay_query,"\\.g"), "", avgexp_query.df$cluster) ###########!!!!!!!!!!!need to print "Assay" into here!!
-
-
-   avgexp_db.df <- avgexp_db.df[c("Row", "celltype", "percentile")]
-   avgexp_query.df <- avgexp_query.df[c("Row", "cluster", "percentile")]
+      avgexp_db.df$celltype <- gsub(paste0("^",assay_db,"\\.g"), "", avgexp_db.df$celltype) ###########!!!!!!!!!!!need to print "Assay" into here!!
+      avgexp_query.df$cluster <- gsub(paste0("^",assay_query,"\\.g"), "", avgexp_query.df$cluster) ###########!!!!!!!!!!!need to print "Assay" into here!!
 
 
-
-   # rearrange dataframes
-
-   avgexp_db.df <- avgexp_db.df %>%
-      pivot_longer(
-         cols = starts_with("percentile"),
-         names_to = "platform",
-         values_to = "expression"
-      ) %>%
-      mutate(platform = "db") %>%
-      unite("platform_celltype", platform, celltype, sep = ".") %>%
-      pivot_wider(
-         names_from = platform_celltype,
-         values_from = expression
-      )
+      avgexp_db.df <- avgexp_db.df[c("Row", "celltype", "percentile")]
+      avgexp_query.df <- avgexp_query.df[c("Row", "cluster", "percentile")]
 
 
-   avgexp_query.df <- avgexp_query.df %>%
-      pivot_longer(
-         cols = starts_with("percentile"),
-         names_to = "platform",
-         values_to = "expression"
-      ) %>%
-      mutate(platform = "query") %>%
-      unite("platform_cluster", platform, cluster, sep = ".") %>%
-      pivot_wider(
-         names_from = platform_cluster,
-         values_from = expression
-      )
+
+      # rearrange dataframes
+
+      avgexp_db.df <- avgexp_db.df %>%
+         pivot_longer(
+            cols = starts_with("percentile"),
+            names_to = "platform",
+            values_to = "expression"
+         ) %>%
+         mutate(platform = "db") %>%
+         unite("platform_celltype", platform, celltype, sep = ".") %>%
+         pivot_wider(
+            names_from = platform_celltype,
+            values_from = expression
+         )
 
 
-   merged.df <- merge(avgexp_db.df, avgexp_query.df, by = "Row", all.x = T, all.y = T)
+      avgexp_query.df <- avgexp_query.df %>%
+         pivot_longer(
+            cols = starts_with("percentile"),
+            names_to = "platform",
+            values_to = "expression"
+         ) %>%
+         mutate(platform = "query") %>%
+         unite("platform_cluster", platform, cluster, sep = ".") %>%
+         pivot_wider(
+            names_from = platform_cluster,
+            values_from = expression
+         )
 
-   merged.df <- column_to_rownames(merged.df, var = "Row")
-   ann@results$marker_free$norm_merge=merged.df
+
+      merged.df <- merge(avgexp_db.df, avgexp_query.df, by = "Row", all.x = T, all.y = T)
+
+      merged.df <- column_to_rownames(merged.df, var = "Row")
+      ann@results$marker_free$norm_merge=merged.df
+   }else if(norm_method == "within_clust"){
+      avgexp_query.df <- as.data.frame(apply(ann@ann3$query, 2, function(x) (x - mean(x)) / sd(x)))
+      avgexp_db.df <- as.data.frame(apply(ann@ann3$db, 2, function(x) (x - mean(x)) / sd(x)))
+
+      colnames(avgexp_query.df) <- paste0("query.",colnames(avgexp_query.df))
+      colnames(avgexp_db.df) <- paste0("db",colnames(avgexp_db.df))
+
+      avgexp_query.df <- avgexp_query.df %>%
+         rownames_to_column(var = "Row")
+      avgexp_db.df <- avgexp_db.df %>%
+         rownames_to_column(var = "Row")
+
+      merged.df <- merge(avgexp_db.df, avgexp_query.df, by = "Row", all.x = T, all.y = T)
+
+      merged.df <- column_to_rownames(merged.df, var = "Row")
+      ann@results$marker_free$norm_merge=merged.df
+
+   }else {
+      warning("Neither normalization across clusters (norm_method = 'across_clust') nor any other valid option detected. Please consult documentation.")
+   }
    #this need to be made into a list
    return(ann)
 
