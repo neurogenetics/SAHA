78f106406c01c62c811609d49ad90a0a05d45771
Richard Mustaklem <rmustak@iu.edu>
2026-03-04 12:53:18 -0500

    Update SAHA.R

--- R/SAHA.R
+++ R/SAHA.R
@@ -17,27 +17,61 @@
 #' @export
 
 SAHA <- function(query, db, meta, data_type) {
-   capture.output({
-      ann <- suppressMessages(Create_SAHA_object(query = query, db = db, data_type = data_type))
-
-      if (data_type == "Markers") {
-         ann <- suppressMessages(Initialize_Markers(ann))
-         ann <- suppressMessages(Tune_Markers(ann = ann, method = "absolute", method_value = 100, method_var = "avg_log2FC", set = "db"))
-         ann <- suppressMessages(Tune_Markers(ann = ann, method = "relative", method_value = 0.75, method_var = "avg_log2FC", set = "query"))
-         ann <- suppressMessages(Run_Marker_Based(ann))
-         ann <- suppressMessages(Create_MarkerBased_Viz(ann, meta = meta))
-         return(call_SAHA_plots(ann, plot_type = "Marker-based", data_type = "Markers"))
-
-      } else if (data_type == "AvgExp") {
-         ann <- suppressMessages(Initialize_MarkerFree(ann = ann))
-         ann <- suppressMessages(Downsample(ann))
-         ann <- suppressMessages(NormalizeDS(ann, assay_query = "RNA"))
-         ann <- suppressMessages(CorrelateDS(ann))
-         ann <- suppressMessages(Create_MarkerFree_Viz(ann, meta = meta))
-         return(call_SAHA_plots(ann, plot_type = "Marker-free", data_type = "AvgExp"))
-
-      } else {
-         cat("Something went wrong. Please read the documentation or consider running the full SAHA pipeline.")
-      }
-   })
+  if (missing(query)) {
+    stop("Argument 'query' is missing, with no default value")
+  }
+  if (missing(db)) {
+    stop("Argument 'db' is missing, with no default value")
+  }
+  if (missing(meta)) {
+    stop("Argument 'meta' is missing, with no default value")
+  }
+  if (missing(data_type)) {
+    stop("Argument 'data_type' is missing, with no default value")
+  }
+  suppressMessages({
+    suppressWarnings({
+      capture.output({
+        
+        if (data_type == "Markers") {
+          ann <- Create_SAHA_object(query = query, db = db, data_type = "Markers")
+          ann <- Initialize_Markers(ann)
+          ann <- Tune_Markers(ann = ann, method = "absolute", method_value = 100, method_var = "avg_log2FC", set = "db")
+          ann <- Tune_Markers(ann = ann, method = "relative", method_value = 0.75, method_var = "avg_log2FC", set = "query")
+          ann <- Run_Marker_Based(ann)
+          ann <- Create_MarkerBased_Viz(ann, meta = meta)
+          return(call_SAHA_plots(ann, plot_type = "Marker-based", data_type = "Markers"))
+        } else if (data_type == "AvgExp") {
+          ann <- Create_SAHA_object(query = query, db = db, data_type = "AvgExp")
+          ann <- Initialize_MarkerFree(ann = ann)
+          ann <- Downsample(ann)
+          ann <- NormalizeDS(ann, assay_query = "RNA")
+          ann <- CorrelateDS(ann)
+          ann <- Create_MarkerFree_Viz(ann, meta = meta)
+          return(call_SAHA_plots(ann, plot_type = "Marker-free", data_type = "AvgExp"))
+        } else if (data_type == "Both") {
+          
+          markers_ann <- Create_SAHA_object(query = query_Markers, db = ISOCTX_Markers, data_type = "Markers")
+          markers_ann <- Initialize_Markers(markers_ann)
+          markers_ann <- Tune_Markers(markers_ann, method = "absolute", method_value = 100, method_var = "avg_log2FC", set = "db")
+          markers_ann <- Tune_Markers(markers_ann, method = "relative", method_value = 0.75, method_var = "avg_log2FC", set = "query")
+          markers_ann <- Run_Marker_Based(markers_ann)
+          markers_ann <- Create_MarkerBased_Viz(markers_ann, meta = meta)
+          markers_result <- call_SAHA_plots(markers_ann, plot_type = "Marker-based", data_type = "Markers")
+    
+          avgexp_ann <- Create_SAHA_object(query = query_AvgExp, db = ISOCTX_AvgExp, data_type = "AvgExp")
+          avgexp_ann <- Initialize_MarkerFree(avgexp_ann)
+          avgexp_ann <- Downsample(avgexp_ann)
+          avgexp_ann <- NormalizeDS(avgexp_ann, assay_query = "RNA")
+          avgexp_ann <- CorrelateDS(avgexp_ann)
+          avgexp_ann <- Create_MarkerFree_Viz(avgexp_ann, meta = meta)
+          avgexp_result <- call_SAHA_plots(avgexp_ann, plot_type = "Marker-free", data_type = "AvgExp")
+          
+          return(invisible(list(markers = markers_result, avgexp = avgexp_result)))
+        } else {
+          stop(paste("Invalid data_type:", data_type, "- must be 'Markers', 'AvgExp', or 'Both'"))
+        }
+      }, type = "output")
+    })
+  })
 }
