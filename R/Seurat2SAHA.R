24df850123d4cbf132cab90e4e00e5ed702c1d0e
Richard Mustaklem <rmustak@iu.edu>
2026-03-12 09:48:13 -0400

    Update Seurat2SAHA.R
    suppress warnings for avgexp and 'both' corrected as issued on github. need someone to double check this as i have not yet

--- R/Seurat2SAHA.R
+++ R/Seurat2SAHA.R
@@ -13,35 +13,35 @@
 #' @export
 #' @importFrom Seurat VariableFeatures AverageExpression FindAllMarkers
 Seurat2SAHA <- function(obj, output="Both"){
-   if (output=="Both") {
-     cat("Calculating Variable Features: \n")
-     varfeat = VariableFeatures(obj)
-     cat(paste0("Storing ",length(varfeat)," variable features. \n"))
-     cat("Calculating Average Expression: \n")
-     avgexp =  suppressMessages(AverageExpression(obj,assays = "RNA",slot="data"))
-     avgexp=data.frame(avgexp)
-     cat(paste0("Storing expression of ",length(rownames(avgexp))," unique features across ",length(colnames(avgexp))," clusters. \n"))
-     cat("Calculating Cluster Markers: \n")
-     markers = FindAllMarkers(obj, only.pos = TRUE)
-     cat(paste0("Storing "),length(unique(markers$gene)), " unique markers across ", length(unique(markers$cluster)), " clusters.")
-     output = list(varfeat=varfeat, avgexp=avgexp, markers=markers)
-   }else if (output == "AvgExp") {
-      cat("Calculating Variable Features: \n")
-      varfeat = VariableFeatures(obj)
-      cat(paste0("Storing ",length(varfeat)," variable features. \n"))
-      cat("Calculating Average Expression: \n")
-      avgexp =  suppressMessages(AverageExpression(obj,assays = "RNA",slot="data"))
-      avgexp=data.frame(avgexp)
-      cat(paste0("Storing expression of ",length(rownames(avgexp))," unique features across ",length(colnames(avgexp))," clusters. \n"))
-      output = list(varfeat=varfeat, avgexp=avgexp)
-   }else if (output == "Markers") {
-      cat("Calculating Variable Features: \n")
-      varfeat = VariableFeatures(obj)
-      cat(paste0("Storing ",length(varfeat)," variable features. \n"))
-      cat("Calculating Cluster Markers: \n")
-      markers = FindAllMarkers(obj, only.pos = TRUE)
-      cat(paste0("Storing "),length(unique(markers$gene)), " unique markers across ", length(unique(markers$cluster)), " clusters.")
-      output = list(varfeat=varfeat, markers=markers)
-   }
-   return(output)
+  if (output=="Both") {
+    cat("Calculating Variable Features: \n")
+    varfeat = VariableFeatures(obj)
+    cat(paste0("Storing ",length(varfeat)," variable features. \n"))
+    cat("Calculating Average Expression: \n")
+    avgexp =  suppressWarnings(suppressMessages(AverageExpression(obj,assays = "RNA",slot="data")))
+    avgexp=data.frame(avgexp)
+    cat(paste0("Storing expression of ",length(rownames(avgexp))," unique features across ",length(colnames(avgexp))," clusters. \n"))
+    cat("Calculating Cluster Markers: \n")
+    markers = FindAllMarkers(obj, only.pos = TRUE)
+    cat(paste0("Storing "),length(unique(markers$gene)), " unique markers across ", length(unique(markers$cluster)), " clusters.")
+    output = list(varfeat=varfeat, avgexp=avgexp, markers=markers)
+  }else if (output == "AvgExp") {
+    cat("Calculating Variable Features: \n")
+    varfeat = VariableFeatures(obj)
+    cat(paste0("Storing ",length(varfeat)," variable features. \n"))
+    cat("Calculating Average Expression: \n")
+    avgexp =  suppressWarnings(suppressMessages(AverageExpression(obj,assays = "RNA",slot="data")))
+    avgexp=data.frame(avgexp)
+    cat(paste0("Storing expression of ",length(rownames(avgexp))," unique features across ",length(colnames(avgexp))," clusters. \n"))
+    output = list(varfeat=varfeat, avgexp=avgexp)
+  }else if (output == "Markers") {
+    cat("Calculating Variable Features: \n")
+    varfeat = VariableFeatures(obj)
+    cat(paste0("Storing ",length(varfeat)," variable features. \n"))
+    cat("Calculating Cluster Markers: \n")
+    markers = FindAllMarkers(obj, only.pos = TRUE)
+    cat(paste0("Storing "),length(unique(markers$gene)), " unique markers across ", length(unique(markers$cluster)), " clusters.")
+    output = list(varfeat=varfeat, markers=markers)
+  }
+  return(output)
 }
